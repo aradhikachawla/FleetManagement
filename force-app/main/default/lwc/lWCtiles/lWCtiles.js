61200ca3ee5ba5d2e0a0aac6c30361bd8f0fa100
aradhikachawla <58751671+aradhikachawla@users.noreply.github.com>
2021-04-10 11:24:41 -0700

    Delete force-app/main/default/lwc/lWCtiles directory

--- force-app/main/default/lwc/lWCtiles/lWCtiles.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-import PleasureBoats from '@salesforce/resourceUrl/PleasureBoats';
-export default class LWCtiles extends LightningElement {
-
-@api busRec;
-
-}
