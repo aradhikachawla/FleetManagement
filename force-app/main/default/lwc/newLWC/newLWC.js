a4dafd25ee71f3cb9f8980b98fbda8cddc4d44af
aradhikachawla <58751671+aradhikachawla@users.noreply.github.com>
2021-04-10 11:24:48 -0700

    Delete force-app/main/default/lwc/newLWC directory

--- force-app/main/default/lwc/newLWC/newLWC.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, wire } from 'lwc';
-import getBuses from '@salesforce/apex/LWCController.getBuses';
-
-export default class NewLWC extends LightningElement {
-
-          @wire(getBuses)
-          buses;
-          
-}
